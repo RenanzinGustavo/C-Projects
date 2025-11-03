51e58c15292a4fd01812e67a2c1766a282ae8c50
Renan Gustavo <renanplays2017@gmail.com>
2025-11-03 17:18:13 -0300

    frequencia ex

--- ex2 frequencia.cpp
+++ ex2 frequencia.cpp
@@ -1,25 +1,3 @@
- /*Exercício 2: Verificação de Nota e Frequência para Aprovação?
-
-Você foi contratado para fazer um sistema simples de avaliação escolar. O programa deve apresentar o seguinte menu:
-
-
-
-Verificar se o aluno foi aprovado
-
-Sair
-
-?Se o usuário escolher a opção 1, o programa deve solicitar a nota final (de 0 a 10) e a frequência (porcentagem de presença, de 0 a 100).
-
-
-O aluno só será aprovado se a nota for maior ou igual a 7 e a frequência for maior ou igual a 75%. Use if else para essa verificação 
-dentro do case 1.
-
-
-
-Se o usuário escolher a opção 2, o programa deve encerrar.
-
-Se for digitada uma opção inválida, exiba a mensagem "Opção inválida!".*/
-
 #include <stdio.h>
 
 int main(){
@@ -61,3 +39,4 @@ int main(){
 
     return 0;
 }
+
